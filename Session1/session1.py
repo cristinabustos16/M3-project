667cf88578c27054c2868d5d6cd5e9244dd91eb9
xianlopez <lopezalvarez.xian@gmail.com>
2016-12-16 00:15:18 +0100

    More modularization

--- Session1/session1.py
+++ Session1/session1.py
@@ -5,9 +5,12 @@ import time
 from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 from sklearn.decomposition import PCA
+import sys
 
 
-def test_system(test_images_filenames, test_labels, clf, SIFTdetector, stdSlr, pca):
+def test_system(test_images_filenames, test_labels, clf, SIFTdetector, stdSlr, pca, \
+                apply_pca, scale):
+# Use the test data to measure the performance of the adjusted classifier.
     numtestimages=0
     numcorrect=0
     for i in range(len(test_images_filenames)):
@@ -16,28 +19,40 @@ def test_system(test_images_filenames, test_labels, clf, SIFTdetector, stdSlr, p
         gray=cv2.cvtColor(ima,cv2.COLOR_BGR2GRAY)
         kpt,des=SIFTdetector.detectAndCompute(gray,None)
         # Scale descriptors:
-        des_scaled = stdSlr.transform(des)
+        if(scale == 1):
+            des = stdSlr.transform(des)
         # Apply PCA to descriptors:
-        des_pca = pca.transform(des_scaled)
+        if(apply_pca == 1):
+            des = pca.transform(des)
         # Classify:
-        predictions = clf.predict(des_pca)
+        predictions = clf.predict(des)
         values, counts = np.unique(predictions, return_counts=True)
         predictedclass = values[np.argmax(counts)]
         print 'image '+filename+' was from class '+test_labels[i]+' and was predicted '+predictedclass
         numtestimages+=1
         if predictedclass==test_labels[i]:
             numcorrect+=1
-    print 'Final accuracy: ' + str(numcorrect*100.0/numtestimages)
+    accuracy = numcorrect*100.0/numtestimages
+    print 'Final accuracy: ' + str(accuracy)
+    return accuracy
 
 
-def train_classifier(X, L):
+def train_classifier(X, L, SVM_options):
+# Train the classifier, given some options and the training data.
     print 'Training the SVM classifier...'
-    clf = svm.SVC(kernel='linear', C=1).fit(X, L)
+    sys.stdout.flush()
+    if(SVM_options.kernel == 'linear'):
+        clf = svm.SVC(kernel='linear', C = SVM_options.C).fit(X, L)
+    elif(SVM_options.kernel == 'rbf'):
+        clf = svm.SVC(kernel='rbf', C = SVM_options.C, gamma = SVM_options.sigma).fit(X, L)
+    else:
+        print 'SVM kernel not recognized!'
     print 'Done!'
     return clf
 
 
 def read_and_extract_features(train_images_filenames, train_labels, SIFTdetector):
+# Read the images and extract the features.
     Train_descriptors = []
     Train_label_per_descriptor = []
     for i in range(len(train_images_filenames)):
@@ -53,57 +68,69 @@ def read_and_extract_features(train_images_filenames, train_labels, SIFTdetector
     return Train_descriptors, Train_label_per_descriptor
 
 
-##############################################################################
+def train_and_test(scale, apply_pca, ncomp_pca, SIFT_nfeatures, SVM_options):
+# Main program, where data is read, features computed, the classifier fit,
+# and then applied to the test data.
+    start = time.time()
 
-start = time.time()
+    # read the train and test files
+    train_images_filenames = cPickle.load(open('train_images_filenames.dat','r'))
+    test_images_filenames = cPickle.load(open('test_images_filenames.dat','r'))
+    train_labels = cPickle.load(open('train_labels.dat','r'))
+    test_labels = cPickle.load(open('test_labels.dat','r'))
 
-# read the train and test files
-train_images_filenames = cPickle.load(open('train_images_filenames.dat','r'))
-test_images_filenames = cPickle.load(open('test_images_filenames.dat','r'))
-train_labels = cPickle.load(open('train_labels.dat','r'))
-test_labels = cPickle.load(open('test_labels.dat','r'))
+    print 'Loaded '+str(len(train_images_filenames))+' training images filenames with classes ',set(train_labels)
+    print 'Loaded '+str(len(test_images_filenames))+' testing images filenames with classes ',set(test_labels)
 
-print 'Loaded '+str(len(train_images_filenames))+' training images filenames with classes ',set(train_labels)
-print 'Loaded '+str(len(test_images_filenames))+' testing images filenames with classes ',set(test_labels)
+    # create the SIFT detector object
+    SIFTdetector = cv2.SIFT(SIFT_nfeatures)
 
-# create the SIFT detector object
-SIFTdetector = cv2.SIFT(nfeatures=100)
+    # read the just 30 train images per class
+    # extract SIFT keypoints and descriptors
+    # store descriptors in a python list of numpy arrays
+    Train_descriptors, Train_label_per_descriptor = \
+        read_and_extract_features(train_images_filenames, train_labels, SIFTdetector)
 
-# read the just 30 train images per class
-# extract SIFT keypoints and descriptors
-# store descriptors in a python list of numpy arrays
-Train_descriptors, Train_label_per_descriptor = \
-    read_and_extract_features(train_images_filenames, train_labels, SIFTdetector)
+    # Transform everything to numpy arrays
+    D=Train_descriptors[0]
+    L=np.array([Train_label_per_descriptor[0]]*Train_descriptors[0].shape[0])
+    for i in range(1,len(Train_descriptors)):
+        D=np.vstack((D,Train_descriptors[i]))
+        L=np.hstack((L,np.array([Train_label_per_descriptor[i]]*Train_descriptors[i].shape[0])))
 
-# Transform everything to numpy arrays
-D=Train_descriptors[0]
-L=np.array([Train_label_per_descriptor[0]]*Train_descriptors[0].shape[0])
-for i in range(1,len(Train_descriptors)):
-	D=np.vstack((D,Train_descriptors[i]))
-	L=np.hstack((L,np.array([Train_label_per_descriptor[i]]*Train_descriptors[i].shape[0])))
+    # Scale input data, and keep the scaler for later:
+    if(scale == 1):
+        stdSlr = StandardScaler().fit(D)
+        D = stdSlr.transform(D)
 
-# Prepare the scaler:
-stdSlr = StandardScaler().fit(D)
-D_scaled = stdSlr.transform(D)
+    # PCA:
+    if(apply_pca == 1):
+        print "Applying principal components analysis..."
+        pca = PCA(n_components = ncomp_pca)
+        pca.fit(D)
+        D = pca.transform(D)
+        print "Explained variance with ", ncomp_pca , \
+            " components: ", sum(pca.explained_variance_ratio_) * 100, '%'
 
-# PCA:
-print "Applying principal components analysis..."
-ncomp_pca = 20
-pca = PCA(n_components = ncomp_pca)
-pca.fit(D_scaled)
-D_pca = pca.transform(D_scaled)
-print "Explained variance with ", ncomp_pca , \
-    " components: ", sum(pca.explained_variance_ratio_) * 100, '%'
+    # Train a linear SVM classifier
+    clf = train_classifier(D, L, SVM_options)
 
-# Train a linear SVM classifier
-clf = train_classifier(D_pca, L)
+    # get all the test data and predict their labels
+    accuracy = test_system(test_images_filenames, test_labels, clf, SIFTdetector, \
+        stdSlr, pca, apply_pca, scale)
+    
+    end=time.time()
+    running_time = end-start
+    print 'Done in '+str(running_time)+' secs.'
+    
+    # Return accuracy and time:
+    return accuracy, running_time
+    
 
-# get all the test data and predict their labels
-test_system(test_images_filenames, test_labels, clf, SIFTdetector, stdSlr, pca)
+class SVM_options_class:
+# Options for SVM classifier.
+    kernel = 'linear'
+    C = 1
+    sigma = 1
 
-end=time.time()
-print 'Done in '+str(end-start)+' secs.'
 
-## 38.78% in 797 secs.
-
-## With 20 principal components: 30.86% in 362 sec,
---
+++ Session1/tune_SVM.py
@@ -0,0 +1,47 @@
+# Launching program with several options for SVM.
+from session1 import SVM_options_class
+from session1 import train_and_test
+
+
+# General options, common in al calls:
+ncomp_pca = 20
+SIFT_nfeatures = 100
+scale = 1
+apply_pca = 1
+
+# SVM options:
+SVM_options = []
+for i in range(4):
+    instance = SVM_options_class()
+    SVM_options.append(instance)
+# Case 1:
+SVM_options[0] = SVM_options_class()
+# Case 2:
+SVM_options[1] = SVM_options_class()
+SVM_options[1].kernel = 'rbf'
+SVM_options[1].sigma = 1
+# Case 3:
+SVM_options[2] = SVM_options_class()
+SVM_options[2].kernel = 'rbf'
+SVM_options[2].sigma = 0.5
+# Case 4:
+SVM_options[3] = SVM_options_class()
+SVM_options[3].kernel = 'rbf'
+SVM_options[3].sigma = 2
+
+
+# Call main program:
+for i in range(4):
+    accuracy, running_time = train_and_test(scale, apply_pca, ncomp_pca, \
+        SIFT_nfeatures, SVM_options[i])
+    filename = 'resultados' + str(i) + '.txt'
+    fid = open(filename, 'w')
+    line = 'accuracy = ' + str(accuracy) + '    running_time = ' + str(running_time)
+    fid.write(line)
+    fid.close()
+
+
+
+
+
+
