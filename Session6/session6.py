b1c7a1f59ed6174f952fc7c9a8a12a486de149ab
XeniaSalinas <salinasxenia@gmail.com>
2017-02-01 17:42:02 +0100

    changes

--- Session6/session6.py
+++ Session6/session6.py
@@ -8,10 +8,10 @@ from keras import backend as K
 train_data_dir='../../Databases/MIT/train'
 val_data_dir='../../Databases/MIT/validation'
 test_data_dir='../../Databases/MIT/test'
-img_width = 224
-img_height=224
+img_width = 128
+img_height=128
 batch_size=32
-number_of_epoch=20
+number_of_epoch=100
 val_samples=400
 
 def preprocess_input(x, dim_ordering='default'):
@@ -68,11 +68,10 @@ test_generator = datagen.flow_from_directory(test_data_dir,
         class_mode='categorical')
 
 #Create the model
-inputs = Input(batch_shape=(None,224,224,3))
+inputs = Input(batch_shape=(None,img_width,img_height,3))
 x = Convolution2D(32, 3, 3, activation='relu', border_mode='same', name='conv1')(inputs)
-x = MaxPooling2D((2, 2), strides=(2, 2), name='pool1')(x)
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same', name='conv2')(x)
-x = MaxPooling2D((2, 2), strides=(2, 2), name='pool2')(x)
+x = Convolution2D(64, 3, 3, activation='relu', border_mode='same', name='conv2')(x)
+x = MaxPooling2D((20, 20), strides=(10, 10), name='pool2')(x)
 x = Flatten(name='flatten')(x)
 x = Dense(1000, activation='relu', name='fc')(x)
 x = Dense(8, activation='softmax',name='predictions')(x)
@@ -85,7 +84,7 @@ model.summary()
 
 #Fit the model
 history=model.fit_generator(train_generator,
-        samples_per_epoch=batch_size*(int(400/batch_size)+1),
+        samples_per_epoch=batch_size*(int(1706/batch_size)+1),
         nb_epoch=number_of_epoch,
         validation_data=validation_generator,
         nb_val_samples=val_samples)
@@ -110,5 +109,5 @@ plt.savefig('loss.jpg')
 
 #Evaluate the model
 result = model.evaluate_generator(test_generator, val_samples=val_samples)
-print('Test acuracy: ' + str(result[1]*100) + '%')
+print('Test accuracy: ' + str(result[1]*100) + '%')
 print('Test loss: ' + str(result[0]))
